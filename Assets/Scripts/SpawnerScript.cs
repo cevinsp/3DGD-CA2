931bb261d980283877dbbb0e86622061382785dd
zekiton <paulnicholasgabriel@gmail.com>
2023-08-24 06:23:23 +0800

    big wave big wave

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject zombieSpawn;
+    public float period = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Spawn());
+        if (period > 3)
+        {
+            StartCoroutine(Spawn());
+            
+            period = 0;
+        }
+        period += UnityEngine.Time.deltaTime;
     }
     IEnumerator Spawn()
     {
-        yield return new WaitForSeconds(5);
-        Instantiate(zombieSpawn, transform.position, Quaternion.identity); 
+        for (int i = 0; i < 8; i++)
+            Instantiate(zombieSpawn, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0);
     }
 }
