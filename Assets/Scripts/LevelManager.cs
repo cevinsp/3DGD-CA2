f655246c35da7717c9a09356b6c9c4e069fd5a45
cevinsp <cevinsp@gmail.com>
2023-08-24 08:33:20 +0800

    pause menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject footsteps;
+    AssaultTrooper assaultTrooper;
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu, footstepsTransform;
+    public static bool isPaused;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || (Input.GetButtonDown("Cancel")))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        footstepsTransform.SetActive(false);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        footstepsTransform.SetActive(true);
+        isPaused = false;
+    }
+}
